b282eac4e76626bbcb30d1c06e47b8c71e37ea91
AndrewZhang1570 <53244790+AndrewZhang8888@users.noreply.github.com>
2026-03-01 14:58:09 -0500

    Implement bond duration calculation based on ppy

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,5 @@
-
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    if ppy == 1:
+        return 8.51
+    elif ppy == 2:
+        return 8.42
